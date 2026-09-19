85cd5bd07f533c76f15a524bfd6d3b9d41ca7702
subreenaraheelshaykh-lab <subreenaraheelshaykh@gmail.com>
2026-09-19 11:59:00 -0700

    push

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ const{createClient} = supabase
 const client = createClient(supabaseUrl, supabaseKey)
 
 console.log(client);
+
+let submit = document.querySelector("#submitbtn")
+
+
+let usersname = document.querySelector("#name")
+let  usersemail = document.querySelector("#email")
+let  userspassword = document.querySelector("#password")
+submit.addEventListener("click", async(event)=>{
+    event.preventDefault()
+    try {
+        const { error } = await client
+  .from('profiles')
+  .insert([{
+    name : usersname.value,
+    email : usersemail.value
+  }])
+    
+    }
+    catch(error){
+        console.log(error)
+    };
+    
+})
